fbf4c80a7de7533e1c89efb155e5c1e8f3d0c52d
Frank <f.kinuthia01@gmail.com>
2023-03-26 06:15:51 +0300

    Run through

---
+++ album.rb
@@ -0,0 +1,29 @@
+class Album
+
+    GENRES = ["Hip-Hop", "Pop", "Jazz"]
+
+    @@album_count = 0
+
+    def initialize
+        @@album_count += 1
+    end
+
+    def release_date=(date)
+      @release_date = date
+    end
+  
+    def release_date
+      @release_date
+    end
+
+    def self.count
+        @@album_count
+    end
+end
+
+Album.new
+Album.new
+Album.new
+
+puts Album.count
+puts Album::GENRES
